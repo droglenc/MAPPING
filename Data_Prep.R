373162d27dd503d64198163baf6a9aa2a80a9db5
Derek Ogle <DerekOgle51@gmail.com>
2022-09-16 16:12:11 -0500

    Daily Update

--- Data_Prep.R
+++ Data_Prep.R
@@ -17,7 +17,7 @@ compareFiles2Info(pin=file.path("Tracks","aaaOriginals"),info)
 ## Sanitize the original gpx files (remove times, update descriptions, etc.)
 ##   that were created after the moddate
 sanitizeTracks(pin=file.path("Tracks","aaaOriginals"),
-               pout="Tracks",trkinfo=info,moddate="2022-08-31")
+               pout="Tracks",trkinfo=info,moddate="2022-09-15")
 ## Combine All Tracks into a single GPX file ... useful for GoogleEarth/Maps
 combineTracks2GPX(pin="Tracks",pout="Data",fnm=project)
 ## Write all tracks to a single CSV
