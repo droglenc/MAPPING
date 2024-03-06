5daaa1da06c14e1fad0ea098bbff97d9509d0863
Derek Ogle <DerekOgle51@gmail.com>
2024-03-06 09:56:04 -0600

    First run after initial changes to gpxhelpers

--- Scripts/dataPrepHelper.R
+++ Scripts/dataPrepHelper.R
@@ -10,30 +10,68 @@ dataPrepHelper <- function(project,basedir=here::here(),verbose=TRUE) {
     dplyr::filter(Project==project) |>
     dplyr::mutate(Connected=paste(Start,End,sep=", ")) |>
     dplyr::select(-Start,-End)
-  write.csv(df_info,paste0(p_data,"/",project,"_trkinfo.csv"),row.names=FALSE)
   
-  ## Check if files in the directory match up with those in the info file
-  gpxhelpers::compareFiles2Info(pin=p_trks_orig,df_info)
+  ## Check if GPX files in the directory match up with those in the info file
+  cli::cli_h1("Checking Track Info file and GPX files")
+  OK <- gpxhelpers::compareFiles2Info(pin=p_trks_orig,df_info)
+  ## If GPX files and info file do not match then abort rest of the process
+  if (!OK) cli::cli_abort("Need to fix issue with GPX files and info file.")
+  ## IF GPX files and info file do match then continue
+
+  ## Determine status of GPX files in pin
+  tmp <- gpxhelpers::findFileStatus(df_info,p_trks_orig,p_trks_sntzd)
+  df_info <- tmp$trkinfo
   
-  ## Sanitize the original gpx files (remove times, update descriptions, etc.)
-  ##   that were created since last sanitized file in pout
-  gpxhelpers::sanitizeTracks(trkinfo=df_info,
-                             pin=p_trks_orig,pout=p_trks_sntzd)
+  ## Isolate the new and modified tracks
+  new_tracks <- dplyr::filter(df_info,status=="NEW")
+  mod_tracks <- dplyr::filter(df_info,status=="MODIFIED")
   
+  ## Determine if there are any new or modified tracks to handle
+  cat("\n")
+  cli::cli_h1("Sanitizing Tracks")
+  if (nrow(new_tracks)==0 & nrow(mod_tracks)==0) {
+    cli::cli_alert_warning("No new or modified tracks to sanitize!!")
+  }  else {
+    ## Write out the new track information file (assures info file is older than
+    ##   the sanitized track files)
+    write.csv(df_info,paste0(p_data,"/",project,"_trkinfo.csv"),row.names=FALSE)
+    
+    ## Remove files from pout that are not listed in the info file
+    if (length(tmp$trks_rmvd)>0) {
+      file.remove(paste0(file.path(pout,junk),".gpx"))
+      cat("\n")
+      cli::cli_alert_info("Files not in info file but in {pin} were removed: {paste(tmp$trks_rmved,collapse=', ')}")
+    }
+    
+    ## Sanitize new and modified tracks
+    if(nrow(new_tracks)==0) cli::cli_alert_info("No new tracks to sanitize.")
+    else gpxhelpers::sanitizeTracks(trkinfo=new_tracks,pin=p_trks_orig,pout=p_trks_sntzd)
+    
+    if(nrow(mod_tracks)==0) cli::cli_alert_info("No modified tracks to sanitize.")
+    else gpxhelpers::sanitizeTracks(trkinfo=mod_tracks,pin=p_trks_orig,pout=p_trks_sntzd)
+  }  
+
   ## Combine All Tracks into a single GPX file ... useful for GoogleEarth/Maps
-  gpxhelpers::combineTracks2GPX(pin=p_trks_sntzd,
-                                pout=p_data,fnm=project)
+  cat("\n")
+  cli::cli_h1("Create Master GPX File")
+  gpxhelpers::makeMasterGPX(tmp,pin=p_trks_sntzd,pout=p_data,fnm=project)
+  #OLD gpxhelpers::combineTracks2GPX(pin=p_trks_sntzd,pout=p_data,fnm=project)
   
   ## Write all tracks to a single CSV
-  dat <- gpxhelpers::writeGPXnInfo2CSV(trkinfo=df_info,
-                                       pin=p_trks_sntzd,pout=p_data,
-                                       fnm=paste0(project,"_trkdat"))
+  cat("\n")
+  cli::cli_h1("Create Master CSV File")
+  dat <- gpxhelpers::makeMasterCSV(tmp,pin=p_trks_sntzd,pout=p_data,
+                                   fnm=paste0(project,"_trkdat"))
+  #OLD dat <- gpxhelpers::writeGPXnInfo2CSV(trkinfo=df_info,
+  #OLD                                      pin=p_trks_sntzd,pout=p_data,
+  #OLD                                      fnm=paste0(project,"_trkdat"))
   
   ## Send completion message
-  if (verbose) {
     cat("\n")
     cli::cli_alert_success("Data preparation for {project} project is complete!")
-  }
+
+  ## Return list of results
   list(trkdat=dat,trkinfo=df_info)
 }
+
 
